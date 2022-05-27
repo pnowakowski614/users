a74c112cb9a3372147b1373c2942076e92244a6a
pnowakowski614 <p.nowakowski416@gmail.com>
2022-05-27 10:37:15 +0200

    refactor(): refactor code to TypeScript

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const tableBody = document.querySelector("tbody");
+const usersURL = 'http://localhost:3000/users';
+const companiesURL = 'http://localhost:3000/companies';
 let usersNumber = 0;
-
 const createCompanyRow = (company) => {
     const row = document.createElement("tr");
     tableBody.appendChild(row);
-
     const companyCell = document.createElement("td");
     const usersCell = document.createElement("td");
-
     row.appendChild(companyCell);
     row.appendChild(usersCell);
-    
     companyCell.textContent = company.name;
-
     return usersCell;
-}
-
+};
 const addUser = (company, usersCell, usersData) => {
-    usersData.forEach( (user) => {
+    usersData.forEach((user) => {
         if (user.uris.company === company.uri) {
-            usersNumber += 1; 
-            usersCell.textContent += `${usersNumber}. ${user.name}, ${user.email}; `
+            usersNumber += 1;
+            usersCell.textContent += `${usersNumber}. ${user.name}, ${user.email}; `;
         }
     });
     usersNumber = 0;
-}
-
+};
 const populateTable = (companiesData, usersData) => {
-    companiesData.forEach( (company) => { 
+    companiesData.forEach((company) => {
         const usersCell = createCompanyRow(company);
         addUser(company, usersCell, usersData);
     });
-}
-
-const loadData = async() => {
+};
+const loadData = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const usersURL = 'http://localhost:3000/users';
-        const usersRes = await fetch(usersURL);
-        const usersData = await usersRes.json();
-
-        const companiesURL = 'http://localhost:3000/companies';
-        const companiesRes = await fetch(companiesURL);
-        const companiesData = await companiesRes.json();
-
+        const usersRes = fetch(usersURL).then(resp => resp.json());
+        const companiesRes = fetch(companiesURL).then(resp => resp.json());
+        let data = yield Promise.all([usersRes, companiesRes]);
+        let usersData = data[0];
+        let companiesData = data[1];
         populateTable(companiesData, usersData);
     }
-    catch(err) {
+    catch (err) {
         console.error(err);
     }
-}
-
+});
 loadData();
---
+++ script.ts
@@ -0,0 +1,69 @@
+const tableBody = document.querySelector("tbody") as HTMLTableSectionElement;
+const usersURL: string = 'http://localhost:3000/users';
+const companiesURL: string = 'http://localhost:3000/companies';
+let usersNumber: number = 0;
+
+interface user {
+    name: string;
+    email: string;
+    uris: {
+        company: string;
+    };
+}
+
+interface company {
+    name: string;
+    uri: string;
+}
+
+
+
+const createCompanyRow = (company: company) => {
+    const row = document.createElement("tr");
+    tableBody.appendChild(row);
+
+    const companyCell = document.createElement("td");
+    const usersCell = document.createElement("td");
+
+    row.appendChild(companyCell);
+    row.appendChild(usersCell);
+    
+    companyCell.textContent = company.name;
+
+    return usersCell;
+}
+
+const addUser = (company: company, usersCell: HTMLTableCellElement, usersData: user[]) => {
+    usersData.forEach( (user) => {
+        if (user.uris.company === company.uri) {
+            usersNumber += 1; 
+            usersCell.textContent += `${usersNumber}. ${user.name}, ${user.email}; `
+        }
+    });
+    usersNumber = 0;
+}
+
+const populateTable = (companiesData: company[], usersData: user[]) => {
+    companiesData.forEach( (company) => { 
+        const usersCell = createCompanyRow(company);
+        addUser(company, usersCell, usersData);
+    });
+}
+
+const loadData = async() => {
+    try {
+        const usersRes = fetch(usersURL).then(resp => resp.json());
+        const companiesRes = fetch(companiesURL).then(resp => resp.json());
+
+        let data: [user[], company[]] = await Promise.all([usersRes, companiesRes])
+        let usersData = data[0];
+        let companiesData = data[1];
+        
+        populateTable(companiesData, usersData);
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
+loadData();
